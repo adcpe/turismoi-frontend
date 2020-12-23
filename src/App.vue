57d6270c2b2f4e3bd40417be20c53eaa929ba97f
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-23 16:06:36 -0500

    Removed unused parts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tour />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tour from './components/Tour.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Tour,
+  },
+};
 </script>
 
 <style>
